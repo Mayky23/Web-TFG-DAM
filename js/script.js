cd9e77bc83a465a81df2836ca15c23536dcbcb7b
Miguel Ángel Roldán de Haro <mayky23dh@gmail.com>
2024-03-19 12:00:04 +0100

    19/03/2024
    - Ajustes del footer CSS
    
    - Añadidas imágenes de Merch
    
    -

---
+++ js/script.js
@@ -0,0 +1,11 @@
+  // Función para el primer botón
+  function irAPaginaLogIn() {
+    // Redirige a la página 1
+    window.location.href = "login.html";
+  }
+
+  // Función para el segundo botón
+  function irAPaginaStore() {
+    // Redirige a la página 2
+    window.location.href = "store.html";
+  }
