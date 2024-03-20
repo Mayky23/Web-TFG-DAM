7ec3073f966a744eda78cf42c428b62026872862
Miguel Ángel Roldán de Haro <mayky23dh@gmail.com>
2024-03-20 12:47:04 +0100

    20/03/2024
    - Se crea la base de la tienda HTML y CSS
    
    - Se termina la pag principal (index)

--- js/script.js
+++ js/script.js
@@ -9,3 +9,19 @@
     // Redirige a la página 2
     window.location.href = "store.html";
   }
+
+// JavaScript para cambiar la imagen cuando se cambie de página
+const carpetaImagenes = "../img/";
+const nombresImagenes = ["default.png", "workers.png", "movil.png"];
+const imagenMostrada = document.getElementById("imagenMostrada");
+const botonesPaginacion = document.querySelectorAll(".splide__pagination__page");
+
+botonesPaginacion.forEach((boton, index) => {
+  boton.addEventListener("click", () => {
+    // Concatenar la carpeta base con el nombre de la imagen
+    const rutaImagen = carpetaImagenes + nombresImagenes[index];
+    // Cambiar la imagen mostrada al hacer clic en un botón de paginación
+    imagenMostrada.src = rutaImagen;
+    imagenMostrada.alt = "Imagen " + (index + 1);
+  });
+});
